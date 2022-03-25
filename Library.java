5b7a80420e9e27857591d7f6468c0d9ec46bdfba
Ryan <83517758+ryanch117@users.noreply.github.com>
2022-03-25 16:16:41 -0400

    I think its done

--- Library.java
+++ Library.java
@@ -1,22 +1,30 @@
 package project;
 import java.util.HashMap;
-
 /**
  *
  * @author Ryan Chowdhury
  */
 public abstract class Library {
-  HashMap<String, String> books = new HashMap<String, String>();
+  public HashMap<String, Double> books = new HashMap<String, Double>();
+  private HashMap<Customer, Integer> customers = new HashMap<Customer, Integer>();
   private void addBook(String bookName, double price){
-      
+      if (books.containsKey(bookName)){
+          System.out.println("Book already exists!");//replace with proper error message later   
+      }else{
+        books.put(bookName, price);      
+      }
   }
   private void removeBook(String bookName){
-  
+      books.remove(bookName);
   }
   public double getPrice(String bookName){
-      return 0;
+      return books.get(bookName);
   }
   public String viewBooks(){
-      return " ";
+      String view = "";
+      for(String key : books.keySet()){
+          view += "Book: " + key + "Price: $" + books.get(key) + "\n";
+      } 
+      return view;
   }
 }
