05557f43dc2c62737c27cca977723fbe447556e7
Ryan <83517758+ryanch117@users.noreply.github.com>
2022-03-25 15:28:34 -0400

    created and set up library file

---
+++ Library.java
@@ -0,0 +1,22 @@
+package project;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Ryan Chowdhury
+ */
+public abstract class Library {
+  HashMap<String, String> books = new HashMap<String, String>();
+  private void addBook(String bookName, double price){
+      
+  }
+  private void removeBook(String bookName){
+  
+  }
+  public double getPrice(String bookName){
+      return 0;
+  }
+  public String viewBooks(){
+      return " ";
+  }
+}
