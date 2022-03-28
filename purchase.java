4c9c333bd3e8f0bf7093bbcb5d392974198adec4
someonesomeon <92482401+someonesomeon@users.noreply.github.com>
2022-03-28 19:21:47 -0400

    Update purchase.java

--- purchase.java
+++ purchase.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaapplication5;
+
+package project;
 import java.util.HashMap;
 
 public class purchase{
