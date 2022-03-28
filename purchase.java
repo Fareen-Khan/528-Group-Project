01f629b57a16876965f92eb501f411229b7367aa
someonesomeon <92482401+someonesomeon@users.noreply.github.com>
2022-03-28 19:20:33 -0400

    Add files via upload

---
+++ purchase.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+import java.util.HashMap;
+
+public class purchase{
+   public int points;
+   public HashMap<String, Double> books = new HashMap<String, Double>();
+   
+   public void purchase(String n)
+   {
+       System.out.println("Total Cost: %d\n", n.getPrice);
+       System.out.println("Points: %d, Status: %s", person.getPoints, person.getStatus);
+      n.removeBook;
+      person.addpoints;
+   }
+   public void rpurchase(String n)
+   {
+       int a,b;
+       a = n.getprice ;
+       while(a>=(person.getPoints/100)){
+           person.getPoints/100;
+            }
+       b = a-person.getPoints/100;
+       System.out.println("Total Cost: %d\n", b);
+       System.out.println("Points: %d, Status: %s", person.getPoints, person.getStatus);
+      n.removeBook;
+      person.addpoints;
+   }
+   update;
+}
+
