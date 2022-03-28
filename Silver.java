73ab64065dd4011b0820b97277842ae36614c43d
Fareen Khan <84217088+Fareen-Khan@users.noreply.github.com>
2022-03-28 19:21:17 -0400

    Silve Status

---
+++ Silver.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author akfar
+ */
+public class Silver extends State {
+    @Override
+    public void forward(Customer c){
+        c.setState(new Gold());
+    }
+    //Does nothing silver has no back state
+    @Override
+    public void backward(Customer c){
+        
+    }
+}
