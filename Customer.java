522d624910aa668e24b4bd2eb8dcdb3ae3f9ab90
Fareen Khan <84217088+Fareen-Khan@users.noreply.github.com>
2022-03-28 19:20:38 -0400

    Customer Class

---
+++ Customer.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author akfar
+ */
+public class Customer extends Library {
+    // ---- INSTANCE VARIABLES ----
+    //Add Purchase Class as an instance variable
+    private String passWord;
+    public String userName; 
+    private int points;
+    public String name;
+    private State myState = new Silver();
+    
+    // ---- CONSTRUCTOR ----
+    public Customer(String name) {
+        this.name = name;
+    }
+    // ---- METHODS ----
+    
+    //Dont need to override viewBooks
+
+    private void addBook(String bookName, double price){
+        //Does nothing in the customer class
+    }
+    private void removeBook(String bookName){
+        //Does nothing as it's in the customer class
+    }
+    public void forward(){
+        myState.forward(this);
+    }
+    public void backward(){
+        myState.backward(this);
+    }
+    public State getState(){
+        return myState;
+    }
+    public void setState(State s){
+        myState = s;
+    }
+}
