2261ed129bccb5c20599309c08912475343f5a6d
Ryan <83517758+ryanch117@users.noreply.github.com>
2022-03-31 19:23:40 -0400

    added books.txt file

--- Book.java
+++ Book.java
@@ -1,5 +1,7 @@
 package bookstoreapp;
 import java.util.ArrayList;
+import java.io.*;
+import java.util.Scanner;
 /**
  *
  * @author Ryan Chowdhury
@@ -23,12 +25,35 @@ public class Book{
         for(Book x : books){
             contains = x.getName().equals(b.getName());
         }
-        if (!contains){books.add(b);}
+        if (!contains){
+            books.add(b);
+            try{
+                FileWriter in = new FileWriter("books.txt");
+                in.write(b.getName() + ", " + b.getPrice());
+                in.close();
+            }catch(IOException e){
+                System.out.println("An error occured");
+            }
+        }    
     }
     private void removeBook(Book b){
+        File f = new File("books.txt");
         for(Book x : books){
             if(x.getName().equals(b.getName())){
                 books.remove(b);
+                try{
+                    Scanner reader = new Scanner(f);
+                    FileWriter in = new FileWriter("books.txt");
+                    while(reader.hasNextLine()){
+                        String s = reader.nextLine();
+                        String[] data = s.split(", ");
+                        if(b.getName().equals(data[0])){
+                            in.write("");
+                        }
+                    }
+                }catch(IOException e){
+                    System.out.println("An error occured.");
+                }
             }
         }   
     }
